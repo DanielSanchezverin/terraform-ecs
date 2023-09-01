689f31c2cea31811feaa4d1d67f2cb2f53bd86f6
danielsanchezverin <danielsanchezverin@gmail.com>
2023-09-01 12:27:47 -0600

    Add default tags in aws provider

--- provider.tf
+++ provider.tf
@@ -1,1 +1,9 @@
-provider "aws" {}
+provider "aws" {
+  default_tags {
+    tags = {
+      Environment   = var.environment
+      terraform     = var.terraform
+      "Cost Center" = var.environment
+    }
+  }
+}
