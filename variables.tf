e02f3a12dcfbcf3304788f9bd44acf93152cec3d
danielsanchezverin <danielsanchezverin@gmail.com>
2023-09-04 01:29:40 -0600

    Add variables.tf

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "environment" {
+  type        = string
+  description = "Environment (prod, stage, dev)"
+}
+
+variable "terraform" {
+  type        = string
+  description = "Resource created with terraform"
+  default     = "True"
+}
+
+variable "vpc_cidr_block" {
+  type        = string
+  description = "VPC CIDR block"
+}
+
+variable "public_subnet_cidr" {
+  type        = list(string)
+  description = "Public subnet CIDR block"
+}
+
+variable "private_subnet_cidr" {
+  type        = list(string)
+  description = "Private subnet CIDR block"
+}
