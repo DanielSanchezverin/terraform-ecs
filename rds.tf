ed08adb9260f1d1449a81685f738f4211887b436
danielsanchezverin <danielsanchezverin@gmail.com>
2023-09-04 03:34:19 -0600

    Add Security Group to RDS

--- rds.tf
+++ rds.tf
@@ -9,5 +9,26 @@ resource "aws_db_instance" "default" {
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 
-  vpc_security_group_ids = [aws_security_group.main.id]
+  vpc_security_group_ids = [aws_security_group.rds.id]
+}
+
+resource "aws_security_group" "rds" {
+  name        = "rds"
+  description = "Allow inbound traffic to RDS"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "Allow traffic from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
